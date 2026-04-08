56e6e8add23324d03b7fd0477e0caead80576979
Chaithra P <chaitra.p2006@gmail.com>
2026-04-08 16:19:05 +0530

    updated port

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@
 //   PharmaShield AI — Frontend Logic
 // ================================================
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = 'http://127.0.0.1:5000';
 
 let medicines        = [];
 let allMedicinesList = [];
